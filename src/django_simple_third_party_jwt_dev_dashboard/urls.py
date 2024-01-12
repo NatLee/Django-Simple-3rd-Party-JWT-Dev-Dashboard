97bab1030a0d19ec1efee8076c7132772de35a25
NatLee <natlee.work@gmail.com>
2024-01-12 15:20:13 +0800

    [update] version and login functionality

--- src/django_simple_third_party_jwt_dev_dashboard/urls.py
+++ src/django_simple_third_party_jwt_dev_dashboard/urls.py
@@ -4,6 +4,6 @@ from django_simple_third_party_jwt_dev_dashboard import views
 
 urlpatterns = [
     path("", views.dashboard, name="dev-dashboard"),
-    path("login", views.jwt_login, name="dev-jwt-login"),
+    path("login", views.login, name="dev-session-login"),
     path("logout", views.session_logout, name="dev-session-logout")
 ]
--- src/django_simple_third_party_jwt_dev_dashboard/views.py
+++ src/django_simple_third_party_jwt_dev_dashboard/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+
 from django.conf import settings as django_settings
+
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.decorators import login_required
+
+from django.views.decorators.csrf import csrf_exempt
+
 from django_simple_third_party_jwt import settings
 from django_simple_third_party_jwt_dev_dashboard.forms import UserForm
 from django_simple_third_party_jwt_dev_dashboard.settings import DEV_DASHBOARD_SETTINGS
@@ -22,7 +33,6 @@ debug = django_settings.DEBUG
 dev_dashboard_setting = {
     'social_google_client_id': settings.SOCIAL_GOOGLE_CLIENT_ID,
     'social_microsoft_client_id': settings.SOCIAL_MICROSOFT_CLIENT_ID,
-    'microsoft_signin_path': settings.MICROSOFT_SIGNIN_PATH,
     'jwt_token_url': jwt_token_url,
     'jwt_refresh_url': jwt_refresh_url,
     'jwt_verify_url': jwt_verify_url,
@@ -42,27 +52,30 @@ def dashboard(request):
         dev_dashboard_setting
     )
 
-def jwt_login(request):
-    dev_dashboard_setting_ = dev_dashboard_setting.copy()
-    dev_dashboard_setting_['form'] = UserForm
-    return render(
-        request,
-        "login.html",
-        dev_dashboard_setting_
-    )
+@csrf_exempt
+def login(request):
+    # override registration login form
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            # login
+            auth_login(request, form.get_user())
+            # redirect
+            return HttpResponseRedirect(reverse('dev-dashboard'))
+    else:
+        form = AuthenticationForm(request)
 
+    # copy settings and add `form`
+    dev_dashboard_setting_copy = dev_dashboard_setting.copy()
+    dev_dashboard_setting_copy['form'] = form
+    
+    return render(request, 'login.html', dev_dashboard_setting_copy)
 
-
-# override session logout
-from django.contrib.auth import logout as auth_logout
-from django.contrib.auth.decorators import login_required
-from django.views.decorators.csrf import csrf_exempt
-from django.urls import reverse
-from django.http import HttpResponseRedirect
 
 @csrf_exempt
 @login_required
 def session_logout(request):
+    # override session logout
     auth_logout(request)
     return HttpResponseRedirect(reverse('dev-dashboard'))
 
